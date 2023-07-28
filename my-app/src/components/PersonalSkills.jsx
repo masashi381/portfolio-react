2d8128313de22ba6197ef9f3add08385d2a14ab4
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-07-28 16:46:29 -0700

    accordion menu set

---
+++ my-app/src/components/PersonalSkills.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function PersonalSkills({ title, content }) {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className="accordion-item">
+      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+        <div>{title}</div>
+        <div>{isActive ? "-" : "+"}</div>
+      </div>
+      {isActive && <div className="accordion-content">{content}</div>}
+    </div>  
+  )
+};
