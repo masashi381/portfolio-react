09829e09ff14ca5cc1a892c41676119a6e49b2c9
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-07-28 15:35:07 -0700

    projects is done

--- my-app/src/components/Main.jsx
+++ my-app/src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from "./Home";
-import About from "./About"
+import About from "./About";
+import Projects from './Projects';
 
 export default function Main(){
   return (
     <>
     <Home/>
     <About/>
+    <Projects/>
     </>
   )
 }
