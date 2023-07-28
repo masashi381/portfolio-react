c38af8999ce2ad89d73e60e337b8350f195549a6
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-07-27 17:12:11 -0700

    about is done

---
+++ my-app/src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About(){
+  return(
+    <>
+    <img src="../src/images/3b631c2e-bfcb-4c2f-8145-fbd6e9c3efc9.jpg" alt="my picture" />
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci corporis laudantium nam velit asperiores nihil doloremque eveniet ullam omnis. Velit veritatis iure deleniti incidunt, possimus dolorum veniam unde sequi ea?</p>
+    </>
+  )
+};
