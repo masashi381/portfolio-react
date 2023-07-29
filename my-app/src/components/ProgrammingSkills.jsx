db200ec17c2fce9972e67ed305e0461677d40396
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-07-28 17:01:13 -0700

    skills is done

--- my-app/src/components/ProgrammingSkills.jsx
+++ my-app/src/components/ProgrammingSkills.jsx
@@ -1,7 +1,11 @@
 export default function ProgrammingSkills() {
   return (
-    <>
-    
-    </>
+    <div>
+    <i className="fa-brands fa-html5"></i>
+    <i className="fa-brands fa-css3-alt"></i>
+    <i className="fa-brands fa-sass"></i>
+    <i className="fa-brands fa-js"></i>
+    <i className="fa-brands fa-react"></i>
+    </div>
   )
 };
