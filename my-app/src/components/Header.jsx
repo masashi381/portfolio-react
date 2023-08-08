6fe177ef2aed73514ad0fcf17d53af1b61817660
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-08-07 17:13:50 -0700

    style scrolldown is done

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
 import { HeaderStyle, HeaderLinkStyle } from "./style/HeaderStyle";
 
-export default function Header(){
+export default function Header({ passSkills, getSkills}){
+  const skillClickedHandler = () => {
+    getSkills.current.scrollIntoView();
+  };
   return (
     <HeaderStyle>
       <nav>
@@ -9,7 +11,7 @@ export default function Header(){
           <li><HeaderLinkStyle to="/">home</HeaderLinkStyle></li>
           <li><HeaderLinkStyle to="/about">About</HeaderLinkStyle></li>
           <li><HeaderLinkStyle to="/#projects">Projects</HeaderLinkStyle></li>
-          <li><HeaderLinkStyle to="/#skills">Skills</HeaderLinkStyle></li>
+          <li><HeaderLinkStyle to="/#skills" onClick={skillClickedHandler} ref={passSkills}>Skills</HeaderLinkStyle></li>
           <li><HeaderLinkStyle to="/work">Work Experience</HeaderLinkStyle></li>
           <li><HeaderLinkStyle to="/contacts">Contacts</HeaderLinkStyle></li>
         </ul>
