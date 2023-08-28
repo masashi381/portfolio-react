15390dd5d11767b1c468c1847459ce576773076a
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-08-28 16:40:25 -0700

    work stye change from css to sass

--- my-app/src/components/WorkAccordion.jsx
+++ my-app/src/components/WorkAccordion.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import { AccordionInner, AccordionFlex,TitleStyle, ToggleStyle, ContentStyle } from "./style/WorkStyle";
+import { AccordionInner } from "./style/WorkStyle";
 
 export default function WorkAccordion({ workTitle, workContent }) {
-  const [isWorkActive, setIsWorkActive] = useState(false);
-  return (
-  <AccordionInner className="accordion-item">
-      <AccordionFlex className="accordion-title" onClick={() => setIsWorkActive(!isWorkActive)}>
-        <TitleStyle>{workTitle}</TitleStyle>
-        <ToggleStyle>{isWorkActive ? "-" : "+"}</ToggleStyle>
-      </AccordionFlex>
-      {isWorkActive && <ContentStyle className="accordion-content">{workContent}</ContentStyle>}
-    </AccordionInner>  
-  )
-};
+	const [isWorkActive, setIsWorkActive] = useState(false);
+	return (
+		<AccordionInner className="accordion-item">
+			<div className="accordion-title" onClick={() => setIsWorkActive(!isWorkActive)}>
+				<h3>{workTitle}</h3>
+				<div className="accordion-btn">{isWorkActive ? "-" : "+"}</div>
+			</div>
+			{isWorkActive && <div className="accordion-content">{workContent}</div>}
+		</AccordionInner>
+	);
+}
--- my-app/src/components/style/WorkStyle.jsx
+++ my-app/src/components/style/WorkStyle.jsx
@@ -1,55 +1,50 @@
 import styled from "styled-components";
 
 export const WorkContainer = styled.div`
-  width: 80vw;
-  height: 80vh;
-  padding-top: 12rem;
-  margin: 0 auto;
-  h3 {
-  font-family: "Mynerve", cursive;
-  font-size: 3rem;
-  color: rgb(106, 109, 113);
-  text-align: center;
-  margin: 3rem 0;
-  }
+	width: 80vw;
+	height: 80vh;
+	padding-top: 12rem;
+	margin: 0 auto;
+	h3 {
+		font-family: "Mynerve", cursive;
+		font-size: 3rem;
+		color: rgb(106, 109, 113);
+		text-align: center;
+		margin: 3rem 0;
+	}
 `;
-
-// export const Accordion = styled.div`
-  
-// `;
 
 // WorkAccordionStyles
 
 export const AccordionInner = styled.div`
-  background-color: rgb(106, 109, 113);
-  color: rgb(255, 255, 255);
-  display: block;
-  margin-bottom: 0.1rem;
-  padding: .1rem;
-  font-size: 1.8rem;
-`;
-
-export const AccordionFlex = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-`;
-
-export const TitleStyle = styled.div`
-  display: block;
-  margin-bottom: 0.1rem;
-  padding: 1rem;
-  font-size: 1.8rem;
-`;
-
-export const ToggleStyle = styled.div`
-margin-right: 1rem;
-`;
-
-export const ContentStyle = styled.div`
-  height: 30rem;
-  white-space: pre-line;
-  background-color: white;
-  color: rgb(106, 109, 113);
-  padding: 1rem;
+	background-color: rgb(106, 109, 113);
+	color: rgb(255, 255, 255);
+	display: block;
+	margin-bottom: 0.1rem;
+	padding: 0.1rem;
+	font-size: 1.8rem;
+	.accordion-title {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		h3 {
+			display: block;
+			margin: 0;
+			margin-bottom: 0.1rem;
+			padding: 1rem;
+			font-size: 2.4rem;
+			color: rgb(255, 255, 255);
+		}
+		.accordion-btn {
+			margin-right: 1rem;
+			cursor: pointer;
+		}
+	}
+	.accordion-content {
+		height: 30rem;
+		white-space: pre-line;
+		background-color: white;
+		color: rgb(106, 109, 113);
+		padding: 1rem;
+	}
 `;
