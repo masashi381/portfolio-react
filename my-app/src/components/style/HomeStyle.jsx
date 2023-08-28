a403a265cad1e3414a84cdefc4f49293851cc9dd
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-08-28 15:51:55 -0700

    home style change from css to sass

--- my-app/src/components/style/HomeStyle.jsx
+++ my-app/src/components/style/HomeStyle.jsx
@@ -2,27 +2,25 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const HomeContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin:  0 3rem 3rem;
-  padding-top: 20rem;
-  height: 100vh;
-`
+	display: flex;
+	justify-content: space-between;
+	margin: 0 3rem 3rem;
+	padding-top: 20rem;
+	height: 100vh;
+	h1 {
+		font-size: 7rem;
+		font-family: "Shantell Sans", cursive;
+	}
+	h2 {
+		font-size: 2.8rem;
+		font-family: "Mynerve", cursive;
+	}
+`;
 
-export const MainTitle = styled.h1`
-  font-size: 7rem;
-  font-family: 'Shantell Sans', cursive;
-
-`
-export const MiniTitle = styled.h2`
-  font-size: 2.8rem;
-  font-family: 'Mynerve', cursive;
-
-`
 export const LinkStyle = styled(Link)`
-  font-size: 2rem;
-  color: rgb(73, 74, 76);
-  &:first-child{
-    margin-right: 2rem;
-  }
-`
+	font-size: 2rem;
+	color: rgb(73, 74, 76);
+	&:first-child {
+		margin-right: 2rem;
+	}
+`;
