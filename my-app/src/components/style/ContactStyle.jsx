85a1da31075ca87e37537ec62b1c25a7d7d9d7a3
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-09-01 16:21:17 -0700

    contacts responsive mobile is done

--- my-app/src/components/style/ContactStyle.jsx
+++ my-app/src/components/style/ContactStyle.jsx
@@ -6,6 +6,10 @@ export const ContactsContainer = styled.div`
 	justify-content: space-between;
 	padding-top: 12rem;
 	margin: 0 2rem 5rem;
+
+	@media screen and (max-width: 375px) {
+		flex-direction: column;
+	}
 `;
 
 // form style
@@ -21,6 +25,7 @@ export const FormStyle = styled.form`
 		margin: 1rem auto;
 		label {
 			font-size: 1.6rem;
+			font-family: "Shantell Sans", cursive;
 			padding-bottom: 1rem;
 			color: rgb(106, 109, 113);
 		}
@@ -40,6 +45,12 @@ export const FormStyle = styled.form`
 	@media screen and (max-width: 820px) {
 		width: 40vw;
 	}
+
+	@media screen and (max-width: 375px) {
+		order: 2;
+		width: 90%;
+    margin-top: 2rem;
+	}
 `;
 
 // SNS style
@@ -49,6 +60,7 @@ export const OptionsStyle = styled.div`
 	width: 33vw;
 	p {
 		font-size: 1.6rem;
+		font-family: "Shantell Sans", cursive;
 		width: 100%;
 		margin-bottom: 5rem;
 		text-align: center;
@@ -65,9 +77,22 @@ export const OptionsStyle = styled.div`
 			font-size: 2rem;
 		}
 	}
+
+	@media screen and (max-width: 375px){
+		order: 1;
+		width: 90%;
+    margin-top: 1rem;
+		p{
+			margin-bottom: 3rem;
+		}
+	}
 `;
 
 export const IconsStyle = styled(Link)`
 	font-size: 2rem;
 	color: rgb(73, 74, 76);
+
+	@media screen and (max-width: 375px){
+		font-size: 3rem;
+	}
 `;
