d3f385f149205ac42f4b0c1ede5614b2c34eef8c
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-09-02 17:00:36 -0700

    hamburgar menu is done

---
+++ my-app/src/components/common/ToggleBtn.jsx
@@ -0,0 +1,18 @@
+import { HamburgerStyle } from "./HeaderStyle";
+
+export default function ToggleBtn(props) {
+	console.log("Toggle", props);
+	return (
+		<HamburgerStyle
+			type="button"
+			aria-controls={props.controls}
+			aria-expanded={props.open}
+			onClick={props.onClick}
+			className="toggleBtn"
+		>
+			<span className="line-1 line"></span>
+			<span className="line-2 line"></span>
+			<span className="line-3 line"></span>
+		</HamburgerStyle>
+	);
+}
